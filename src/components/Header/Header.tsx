a31810610b2cde3650ee482c1837a1faf1009c77
Lucas Borgatta <lucasborgatta00@outlook.com>
2025-06-19 12:59:25 -0300

    New sideBar and update header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,23 +2,42 @@ import type { Dispatch, SetStateAction } from "react";
 import sun_icon from "./../../assets/sun_icon.svg";
 import moon_icon from "./../../assets/moon_icon.svg";
 import "./Header.css";
-import { Tooltip } from "@mui/material";
+import { Tooltip, useMediaQuery } from "@mui/material";
+import SideBar from "../SideBar/SideBar";
 
 // src/components/Header.jsx
 function Header({ darkMode, setDarkMode }: { darkMode: boolean; setDarkMode: Dispatch<SetStateAction<boolean>> }) {
+  const isMobile = useMediaQuery("(max-width: 768px)");
+
   return (
     <header>
-      <h1>Borgatta Lucas</h1>
-      <div className="header-container">
-        <Tooltip title={darkMode ? "Modo claro" : "Modo oscuro"} placement="left">
-          <button className="dark-mode-button" onClick={() => setDarkMode((prev) => !prev)}>
-            {darkMode ? <img src={sun_icon} width={18} height={18} color="#fff" /> : <img src={moon_icon} width={18} height={18} color="#fff" />}
-          </button>
-        </Tooltip>
-        <button>Proyectos</button>
-        <button>Sobre mí</button>
-        <button>Contacto</button>
-      </div>
+      {isMobile ? (
+        <>
+          <SideBar></SideBar>
+          <h1>Borgatta Lucas</h1>
+          <div className="header-container">
+            <Tooltip title={darkMode ? "Modo claro" : "Modo oscuro"} placement="left">
+              <button className="dark-mode-button" onClick={() => setDarkMode((prev) => !prev)}>
+                {darkMode ? <img src={sun_icon} width={18} height={18} color="#fff" /> : <img src={moon_icon} width={18} height={18} color="#fff" />}
+              </button>
+            </Tooltip>
+          </div>
+        </>
+      ) : (
+        <>
+          <h1>Borgatta Lucas</h1>
+          <div className="header-container">
+            <Tooltip title={darkMode ? "Modo claro" : "Modo oscuro"} placement="left">
+              <button className="dark-mode-button button" onClick={() => setDarkMode((prev) => !prev)}>
+                {darkMode ? <img src={sun_icon} width={18} height={18} color="#fff" /> : <img src={moon_icon} width={18} height={18} color="#fff" />}
+              </button>
+            </Tooltip>
+            <button className="button">Proyectos</button>
+            <button className="button">Sobre mí</button>
+            <button className="button">Contacto</button>
+          </div>
+        </>
+      )}
     </header>
   );
 }
---
+++ src/components/SideBar/SideBar.tsx
@@ -0,0 +1,47 @@
+import { Box, Drawer, IconButton, List, ListItem, ListItemButton, ListItemText } from "@mui/material";
+import { useState } from "react";
+import DehazeIcon from "@mui/icons-material/Dehaze";
+import './SideBar.css'
+
+function SideBar() {
+  const [open, setOpen] = useState(false);
+
+  const toggleDrawer = (newOpen: boolean) => () => {
+    setOpen(newOpen);
+  };
+
+  const DrawerList = (
+    <Box sx={{ width: 250 }} role="presentation" onClick={toggleDrawer(false)}>
+      <List>
+        <ListItem disablePadding>
+          <ListItemButton>
+            <ListItemText primary={"Proyectos"} />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton>
+            <ListItemText primary={"Sobre mí"} />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton>
+            <ListItemText primary={"Contacto"} />
+          </ListItemButton>
+        </ListItem>
+      </List>
+    </Box>
+  );
+
+  return (
+    <div className="sideBar-container">
+      <IconButton onClick={toggleDrawer(true)}>
+        <DehazeIcon />
+      </IconButton>
+      <Drawer open={open} onClose={toggleDrawer(false)}>
+        {DrawerList}
+      </Drawer>
+    </div>
+  );
+}
+
+export default SideBar;
